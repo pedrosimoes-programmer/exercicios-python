46110fe46222c8e737948b96fb7a10922438970e
Pedro Simões <pedrobovo1234@gmail.com>
2020-05-02 18:40:13 -0300

    ex. 100 resolvido

--- exercicios-Python/ex100.py
+++ exercicios-Python/ex100.py
@@ -1,13 +1,25 @@
 from random import randint
+from time import sleep
 
 
 def sorteia(num):
-    numeros = []
+    print(f'Sorteando os 5 valores: ', end='')
     for num in range(0, 5):
-        numeros.append(randint(1, 10))
+        n = randint(1, 10)
+        numeros.append(n)
+        print(f'{n}', end=' ', flush=True)
+        sleep(0.3)
+    print('\nFIM DO SORTEIO!')
 
 
+def somaPar(numLista):
+    somapares = 0
+    for numero in numLista:  # Poderia ser < 5, pois a lista contém 5 números
+        if numero % 2 == 0:
+            somapares += numero
+    print(f'A soma dos números pares sorteados foi: {somapares}.')
 
-numeros = list()
+
+numeros = []
 sorteia(numeros)
-print(numeros)
+somaPar(numeros)
