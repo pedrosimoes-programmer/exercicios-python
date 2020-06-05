aa70e0dbbb145a422ac28780c2645f8a106704f0
Pedro Simões <pedrobovo1234@gmail.com>
2020-06-05 18:11:53 -0300

    Update ex103.py

--- exercicios-Python/ex103.py
+++ exercicios-Python/ex103.py
@@ -1,9 +1,15 @@
-def ficha(* nomeJogador, gols ):   
+def ficha(nomeJogador='<desconhecido', gols=0):   
     print(f'O jogador {nomeJogador} fez {gols} gol(s) no campeonato!')
-    
 
 #Programa Principal
 
 jogador = str(input('Informe o nome do jogador: '))
-totGols = int(input(f'Informe quantos gols o jogador fez: '))   
-ficha(jogador, totGols)
+totGols = str(input('Informe quantos gols o jogador fez: '))   
+if totGols.isnumeric():
+    totGols = int(totGols)
+else:
+    totGols = '0'
+if jogador.strip() == '':
+    ficha(gols=totGols)
+else:
+    ficha(jogador, totGols)
