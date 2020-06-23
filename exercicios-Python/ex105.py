7a05e83a3d3f3fd1eae817e39dc04bcd4978fac1
Pedro Simões <pedrobovo1234@gmail.com>
2020-06-23 00:07:54 -0300

    Update ex105.py

--- exercicios-Python/ex105.py
+++ exercicios-Python/ex105.py
@@ -1,4 +1,12 @@
 def notas(*notes, sit=False):
+    #DOC STRING 
+    """
+    -> Função para analisar notas e situações de diversos alunos;
+    :param notes = uma ou mais notas recebidas (aceita várias);
+    :param sit = valor opcional que indica se deve ou não adicionar a situação;
+    :return: dicionários com várias informações sobre as notas dos alunos;
+    
+    """
     dicionario = {}
     dicionario['notas'] = notes #(RETIRAR AO FINAL DA FUNÇÃO!)
     maiorNota = menorNota = cont = 0
@@ -12,14 +20,14 @@ def notas(*notes, sit=False):
             if dicionario['notas'][cont] < menorNota:
                 menorNota = dicionario['notas'][cont]
         cont += 1
-    dicionario['maior'] = maiorNota
-    dicionario['menor'] = menorNota
+    dicionario['maior'] = maiorNota  # PODERIA TER USADO O PARÂMETRO MAX(notes)
+    dicionario['menor'] = menorNota  # PODERIA TER USADO O PARÂMETRO MIN(notes)
     somaNotas = sum(dicionario['notas'])
     dicionario['média'] = somaNotas / dicionario['total']
     if sit == True:     #OPCIONAL (RETIRAR CASO NÃO HOUVER AO FINAL DA FUNÇÃO!)
         if dicionario['média'] < 6:
             dicionario['situacao'] = 'RUIM'
-        if dicionario['média'] >= 6 and dicionario['média'] < 7:
+        if dicionario['média'] >= 5 and dicionario['média'] < 7:
             dicionario['situacao'] = 'RAZOÁVEl'
         if dicionario['média'] > 7:
             dicionario['situacao'] = 'BOA'
@@ -29,3 +37,4 @@ def notas(*notes, sit=False):
 # Programa principal
 resp = notas(1.5, 2.5, 10, 10, sit=True)
 print(resp)
+help(notas)
